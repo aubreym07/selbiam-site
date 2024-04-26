a78ddd18a556f79ab1d39ec077b6e6bafcec3534
Aubrey Macasaet <aubreymacasaet@gmail.com>
2024-04-26 21:15:45 +0800

    initial updates

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h, computed } from 'vue';
+import { createInertiaApp, usePage } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import "vue3-toastify/dist/index.css";
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        const captcheKey = props.initialPage.props.recaptcha_site_key;
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
